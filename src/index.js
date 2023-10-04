8590b0d5d44cdcff60b100ea793b7bf26d2536d4
6sous <cyrille.aquilina@gmail.com>
2023-10-04 14:07:02 +0200

    toggle button done. Added some css

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector(".toggle-btn");
+// My Github location.replace("https://github.com/Sepehr-Aghdasi82");
+
+btn.addEventListener("click", () => {
+  btn.classList.toggle("active");
+});
