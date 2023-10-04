f043c7f25980955cdcb6f2ee9e651913be08205c
ArthurHtbk <arthur.heurtebise@gmail.com>
2023-10-04 12:29:04 +0200

    French text in a separate object

---
+++ src/LanguageContent.js
@@ -0,0 +1,87 @@
+const languageContent = [
+  {
+    fr: {
+      navbar: ["Accueil", "Personnages", "Dan Harmon"],
+      hero: {
+        title: "Rick et Morty",
+        desc: "Bienvenue dans l'univers de Rick et Morty",
+      },
+      characters: {
+        rickSanchez: [
+          "Rick Sanchez est l'un des personnages les plus mémorables et intrigants de l'univers de la télévision animée, et il est le protagoniste de la série à succès \"Rick and Morty\". Créé par Dan Harmon et Justin Roiland, Rick est un scientifique génial, mais aussi profondément tourmenté, dont les aventures interdimensionnelles ont captivé le public depuis la première diffusion de la série en 2013.",
+          "Rick est un homme d'un âge avancé aux cheveux bleus ébouriffés, portant généralement une blouse blanche, et il est presque toujours vu avec une bouteille d'alcool à la main. Son apparence décontractée contraste fortement avec son intellect brillant et sa personnalité décalée. Il est le père de Beth Smith, le grand-père de Morty Smith et le beau-père de Jerry Smith, et sa relation tumultueuse avec sa famille constitue un élément central de l'intrigue de la série.",
+          "L'histoire de Rick commence avec son retour inattendu dans la vie de sa fille, Beth, après une longue absence. Il s'installe chez elle et emmène rapidement son petit-fils Morty dans une série d'aventures interdimensionnelles, grâce à son invention la plus célèbre, le \"Portal Gun\". Ce dispositif lui permet de voyager entre différentes réalités et dimensions de l'univers à volonté, créant ainsi des opportunités pour des escapades scientifiques et des explorations folles.",
+          "Cependant, derrière cette façade excentrique se cache un passé troublé. On découvre progressivement que Rick a quitté sa famille des années auparavant pour poursuivre ses recherches et ses voyages interdimensionnels. Cette décision a laissé des cicatrices profondes chez Beth, qui a toujours cherché à gagner l'approbation de son père. Elle a également eu un impact sur Morty, qui est souvent exposé à des dangers et des horreurs indicibles lors de ses aventures avec Rick.",
+          "Une des caractéristiques les plus frappantes de Rick est son mépris pour l'autorité et les conventions sociales. Il se moque ouvertement des gouvernements, des lois et des normes morales, et il ne recule devant rien pour satisfaire sa curiosité scientifique. Cela le place souvent en conflit avec des entités puissantes, notamment le Conseil de l'Interdimensionnel, une organisation qui surveille les voyages entre les dimensions.",
+          "Au fil de la série, Rick Sanchez se révèle comme un personnage complexe, à la fois tourmenté et profondément intelligent. Malgré ses tendances autodestructrices et son penchant pour l'irrévérence, il montre parfois des signes d'affection envers Morty et semble éprouver de la culpabilité pour avoir abandonné sa fille Beth et sa mère.",
+          "L'histoire de Rick est également marquée par des moments de vulnérabilité et de réflexion profonde. Il remet en question l'existence de Dieu, la nature de la réalité et les dilemmes philosophiques fondamentaux, créant ainsi des dialogues existentiels captivants dans la série.",
+          "En fin de compte, l'histoire de Rick Sanchez est une exploration complexe de la nature humaine, de la famille, de la science, de la folie et de l'existentialisme. Son personnage continue à évoluer au fil des saisons de \"Rick and Morty\", apportant son lot de surprises et de rebondissements. Rick Sanchez restera sans aucun doute un personnage inoubliable et fascinant de la télévision animée contemporaine, prouvant que même au milieu des univers infinis, les luttes personnelles et les questionnements existentiels sont des thèmes universels.",
+        ],
+        mortySmith: [
+          "Morty Smith est le petit-fils de Rick Sanchez, le scientifique génial et excentrique de la série, et il occupe une place unique dans l'univers chaotique de l'émission. Avec ses caractéristiques distinctives, Morty est devenu un personnage emblématique de la culture populaire depuis le lancement de la série en 2013.",
+
+          "Morty est un adolescent typique en apparence, avec des cheveux châtains, des lunettes et un style vestimentaire ordinaire. Son apparence générale contraste fortement avec la personnalité chaotique de son grand-père Rick. Il est souvent vu portant une expression d'anxiété et d'incertitude, ce qui est compréhensible étant donné les aventures interdimensionnelles dangereuses et imprévisibles auxquelles il est régulièrement exposé.",
+
+          "L'histoire de Morty commence lorsque Rick revient dans la vie de sa famille après une longue absence. Morty, qui était jusqu'alors un adolescent ordinaire, se retrouve entraîné dans les voyages interdimensionnels et les expériences scientifiques folles de son grand-père. Rick l'entraîne souvent dans des situations extrêmement périlleuses, ce qui contraste fortement avec la nature insouciante et irresponsable de Rick lui-même.",
+
+          "Malgré son caractère effrayé et son manque de confiance en lui, Morty développe des compétences au fil du temps. Il devient plus résilient et adaptatif, apprenant à naviguer dans les défis constants auxquels il est confronté. Sa relation avec Rick est complexe, alternant entre moments de frustration et de colère, mais aussi d'affection et de reconnaissance. Morty éprouve à la fois de la peur et de l'admiration pour son grand-père, et cette dynamique est au cœur de l'intrigue de la série.",
+
+          "Une des caractéristiques les plus frappantes de Morty est sa vulnérabilité émotionnelle. Il est souvent déchiré entre ses responsabilités familiales et ses aventures interdimensionnelles avec Rick. Les pressions de sa vie quotidienne, de l'école et de sa famille s'ajoutent au stress constant de ses voyages avec Rick, créant un cocktail d'émotions complexes.",
+
+          "Au fil des saisons, Morty évolue en tant que personnage. Il développe une certaine confiance en lui, bien que son anxiété demeure un élément central de sa personnalité. Il apprend également à remettre en question les actions de Rick et à défendre ses propres valeurs morales, ce qui crée des conflits entre les deux personnages.",
+
+          "En fin de compte, Morty Smith incarne le protagoniste tourmenté et vulnérable de \"Rick and Morty\". Sa relation avec Rick, ses aventures interdimensionnelles et ses luttes personnelles sont au cœur de l'histoire de la série. Malgré ses peurs et ses doutes, Morty est devenu un personnage emblématique de la télévision contemporaine, reflétant les réalités de la croissance et de l'exploration de soi, même au milieu d'univers infinis et d'extravagances scientifiques. Son personnage complexe et évolutif continue de captiver les fans du monde entier, faisant de lui une figure inoubliable de \"Rick and Morty\".",
+        ],
+        summerSmith: [
+          "Summer Smith est la sœur aînée de Morty Smith et la petite-fille de Rick Sanchez, ce qui la place au cœur de l'action dans les aventures interdimensionnelles et les bizarreries scientifiques qui caractérisent la série. Depuis le début de la série en 2013, Summer a évolué en un personnage complexe et fascinant qui explore les défis de l'adolescence et de l'identité dans un contexte exceptionnel.",
+
+          "Summer est une adolescente typique, avec des cheveux roux et un look à la mode. Contrairement à son frère Morty, qui est souvent en proie à l'anxiété, Summer affiche une confiance en soi plus prononcée et un sens de l'aventure qui se développe au fil de la série. Elle est souvent attirée par les expériences interdimensionnelles de Rick, cherchant à échapper à l'ennui de la vie quotidienne.",
+
+          "L'histoire de Summer débute avec le retour de Rick dans la vie de sa famille après une longue absence. Elle se retrouve rapidement entraînée dans les aventures chaotiques de son grand-père, qui la confrontent à des défis inimaginables pour une adolescente. Malgré les dangers et les excentricités auxquels elle est exposée, Summer montre une résilience et une détermination croissantes au fil des saisons.",
+
+          "Une des caractéristiques les plus marquantes de Summer est sa quête d'identité et sa recherche de sa place dans un monde qui ne cesse de se dérober sous ses pieds. Elle doit jongler entre les responsabilités familiales, l'école, les amitiés et les aventures interdimensionnelles, ce qui crée un mélange complexe de frustrations et de découvertes.",
+
+          "Au cours de la série, Summer évolue en tant que personnage. Elle apprend à naviguer dans les bizarreries de l'univers de Rick et à gagner en confiance en elle. Son lien avec Morty et son admiration pour Rick évoluent également, passant de simples relations familiales à des dynamiques complexes, mêlées d'amour, de frustration et de compréhension.",
+
+          "Summer devient de plus en plus un personnage indépendant, capable de prendre des décisions éclairées et de défendre ses propres convictions. Elle n'hésite pas à remettre en question les actions de Rick et à défendre ce en quoi elle croit, ce qui crée parfois des tensions au sein de la famille Smith.",
+
+          "En fin de compte, Summer Smith incarne la complexité de l'adolescence dans un contexte extraordinaire. Son parcours de découverte de soi, ses défis personnels et ses aventures interdimensionnelles font d'elle un personnage fascinant et dynamique de \"Rick and Morty\". Elle représente la capacité de l'individu à grandir, à s'adapter et à affronter les défis, même au milieu des univers infinis et des expériences scientifiques folles. Summer continue d'évoluer et de captiver les fans du monde entier, confirmant son statut d'élément essentiel de l'univers de la série.",
+        ],
+        bethSmith: [
+          "Beth Smith est la fille de Rick Sanchez et la mère de Morty et Summer. Tout au long de la série, Beth occupe une place centrale en tant que membre de la famille Smith, et son caractère complexe offre une perspective unique sur les aventures interdimensionnelles et les défis existentiels qui caractérisent la série.",
+
+          "Beth est une femme d'âge moyen, aux cheveux blonds, portant généralement une blouse de médecin en raison de sa profession. Elle est vétérinaire, une profession qui reflète son amour pour les animaux et sa capacité à gérer les situations difficiles. Cependant, sa vie est loin d'être simple en raison de la présence de son père, Rick, dont les expériences interdimensionnelles imprévisibles et les comportements autodestructeurs la plongent régulièrement dans le chaos.",
+
+          "L'histoire de Beth est marquée par la relation complexe qu'elle entretient avec son père, Rick. Rick a quitté sa famille des années auparavant pour poursuivre ses voyages interdimensionnels et ses expériences scientifiques. Cette absence a créé des cicatrices profondes chez Beth, qui a toujours cherché à gagner l'approbation et l'amour de son père. Le retour de Rick dans sa vie a ravivé ces conflits émotionnels, créant une dynamique tendue au sein de la famille Smith.",
+
+          "La vie de Beth est également influencée par sa relation avec son mari, Jerry Smith. Jerry est l'archétype du gendre typique, bien loin de l'intelligence et de l'excentricité de Rick. Leur mariage est souvent tumultueux, mais Beth tente de maintenir un équilibre entre les besoins contradictoires de sa famille.",
+
+          "En tant que mère, Beth est confrontée à des défis complexes liés à l'éducation de Morty et Summer, qui sont régulièrement exposés à des dangers et des horreurs indicibles lors de leurs aventures avec Rick. Elle doit jongler entre ses responsabilités familiales et sa carrière professionnelle, ce qui ajoute une dimension de stress à sa vie déjà tumultueuse.",
+
+          "Au fil de la série, Beth évolue en tant que personnage. Elle explore sa propre identité, sa relation avec son père et ses choix de vie. Elle est confrontée à des dilemmes moraux et existentiels qui remettent en question les fondements de la réalité et de la moralité. Ces questions complexes ajoutent une profondeur inattendue au personnage de Beth.",
+
+          'Beth Smith incarne la lutte humaine pour trouver un sens dans un univers apparemment absurde et chaotique. Sa quête pour comprendre sa place dans le monde et ses relations familiales tumultueuses constituent un fil conducteur puissant de la série "Rick and Morty". Beth représente la complexité de la condition humaine, avec ses aspirations, ses conflits intérieurs et sa recherche de sens.',
+
+          'En fin de compte, Beth Smith est un personnage essentiel de "Rick and Morty". Son parcours personnel, sa quête d\'identité et sa lutte pour trouver un équilibre entre ses responsabilités familiales et sa propre individualité la rendent profondément humaine. Elle continue à évoluer au fil des saisons de la série, apportant une dimension émotionnelle et philosophique qui renforce l\'impact global de l\'émission. Beth Smith reste un exemple frappant de la richesse des personnages et des thèmes explorés dans "Rick and Morty".',
+        ],
+        jerrySmith: [
+          "Jerry Smith est le père de Summer et Morty, mari de Beth Smith et gendre de Rick Sanchez. Jerry occupe une place centrale dans les aventures interdimensionnelles et les dilemmes existentiels qui caractérisent la série. Malgré son apparente simplicité, Jerry est un personnage complexe qui incarne la banalité face à l'extraordinaire.",
+
+          "Jerry est un homme d'âge moyen, au physique ordinaire, et il travaille dans une entreprise de publicité. Son apparence, sa personnalité et ses ambitions sont tout à fait typiques, ce qui contraste fortement avec les membres excentriques de sa famille, en particulier Rick, le grand-père de ses enfants. Jerry représente la normalité dans un univers où tout est extraordinaire.",
+
+          "L'histoire de Jerry est marquée par sa relation tumultueuse avec Rick, un scientifique génial et erratique qui vit avec sa famille. Jerry est souvent en conflit avec Rick en raison de leurs différences fondamentales : Rick rejette la société, la science conventionnelle et les conventions sociales, tandis que Jerry aspire à la stabilité, à la sécurité et à la normalité. Leurs désaccords réguliers créent des tensions au sein de la famille Smith.",
+
+          "En tant que père de famille, Jerry est confronté à des défis constants. Il doit équilibrer les responsabilités parentales avec les aventures interdimensionnelles de Rick, ce qui crée un mélange complexe de frustration et d'admiration envers son beau-père. Malgré ses tentatives pour être un bon père, Jerry est souvent maladroit et peu compétent, ce qui ajoute des éléments comiques à la série.",
+
+          "Sa relation avec sa femme, Beth Smith, est également un élément central de son personnage. Beth est la fille de Rick et la mère de leurs enfants, Morty et Summer. Le mariage de Jerry et Beth est souvent tumultueux en raison des influences de Rick sur leur vie. Cependant, malgré leurs désaccords et leurs disputes, Jerry et Beth partagent un lien profond et ont une histoire d'amour complexe.",
+
+          "Jerry est un personnage qui évolue au fil de la série. Il se trouve souvent en situation d'échec, mais il apprend à se débrouiller dans l'univers chaotique de Rick et à trouver sa place dans la famille. Malgré ses défauts, Jerry a ses moments de perspicacité et de compassion, ce qui montre sa capacité à apprendre et à grandir.",
+
+          "En fin de compte, Jerry Smith est le reflet de la lutte pour la normalité dans un monde extraordinaire. Son caractère ordinaire et ses aspirations modestes contrastent avec les éléments extravagants de l'univers de \"Rick and Morty\". Jerry est un personnage qui incarne la résilience face à l'adversité, la recherche de la stabilité au milieu du chaos et la quête d'acceptation au sein d'une famille dysfonctionnelle.",
+
+          "Jerry Smith continue d'être un élément essentiel de \"Rick and Morty\". Sa capacité à représenter la normalité dans un monde extraordinaire apporte une dimension d'humour, de réflexion et de réalisme à la série. Jerry est un rappel que même dans les mondes les plus étranges et les aventures les plus folles, il y a de la place pour la simplicité et la recherche de sens.",
+        ],
+      },
+    },
+  },
+];
